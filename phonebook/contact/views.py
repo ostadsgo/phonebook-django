952a59d59cc0edee57224b0659f3711de75223fb
Saeed Gholami <saeed.ghollami@gmail.com>
2025-07-23 18:34:29 +0330

    pagination.

--- phonebook/contact/views.py
+++ phonebook/contact/views.py
@@ -1,10 +1,16 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from .models import Contact
 
 
 def index(request):
-    contacts = Contact.objects.all()
+    query = request.GET.get('q')
+    if query:
+        contact_list = Contact.objects.filter(employee_name__icontains=query) # type: ignore
+    else:
+        contact_list = Contact.objects.all()  # type: ignore
+
     table_header = [
         "نام کارمند",
         "نام سازمان",
@@ -13,8 +19,23 @@ def index(request):
         "شماره داخلی",
         "شماره تماس",
     ]
+
+    # Paginator number
+    pg_num = request.GET.get("pg_num", 10)
+
+    # Paginator
+    paginator = Paginator(contact_list, int(pg_num))
+    page_number = request.GET.get("page")
+    contacts = paginator.get_page(page_number)
+    
+
+    print('-' * 50)
+    print(request.GET)
+    print('-' * 50)
+
+
     return render(
         request,
         "contact/index.html",
-        {"contacts": contacts, "table_header": table_header},
+        {"contacts": contacts, "table_header": table_header, "pg_num": pg_num},
     )
